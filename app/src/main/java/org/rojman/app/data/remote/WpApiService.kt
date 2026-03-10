b61876a19eecb06d78c8150607c3b35616c32718
saeedzadehzana-cyber <saeedzadehzana@gmail.com>
2026-03-10 22:33:42 +0100

    Update WpApiService.kt

--- app/src/main/java/org/rojman/app/data/remote/WpApiService.kt
+++ app/src/main/java/org/rojman/app/data/remote/WpApiService.kt
@@ -1,20 +1,21 @@
 package org.rojman.app.data.remote
 
-import org.rojman.app.data.model.WpCategoryDto
-import org.rojman.app.data.model.WpPostDto
+import org.rojman.app.data.model.WpCategory
+import org.rojman.app.data.model.WpPost
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WpApiService {
-    @GET("wp-json/wp/v2/posts?_embed=1")
-    suspend fun getPosts(
-        @Query("per_page") perPage: Int = 20,
-        @Query("categories") categoryId: Int? = null,
-        @Query("search") search: String? = null
-    ): List<WpPostDto>
 
-    @GET("wp-json/wp/v2/categories")
+    @GET("wp/v2/posts")
+    suspend fun getPosts(
+        @Query("page") page: Int = 1,
+        @Query("per_page") perPage: Int = 10,
+        @Query("categories") categoryId: Int? = null
+    ): List<WpPost>
+
+    @GET("wp/v2/categories")
     suspend fun getCategories(
         @Query("per_page") perPage: Int = 100
-    ): List<WpCategoryDto>
+    ): List<WpCategory>
 }
